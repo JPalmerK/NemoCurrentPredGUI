c2ca2a7bd827de42ce9bcec43a439144aab7e444
KJPalmer <kjp@smruconsulting.com>
2022-10-18 14:58:01 -0700

    ggplot modifications interact with shinyapps.io

--- Code/pullNEMOData.R
+++ Code/pullNEMOData.R
@@ -38,70 +38,62 @@ mps2knots = 1/knots2mps
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-
-    titlePanel( title =  'UBC Tidal Data Forecast'), 
+  
+  # Application title
+  
+  titlePanel( title =  'SalishSeaCast Current Forecast - Haro and Boundary'), 
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    # Set to custom time 
     
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-      # Set to custom time 
-
+    sidebarPanel(
+      # Input: Choose dataset ----
       
+      selectInput("dataset", "Choose a dataset:",
+                  choices = metaDf$locs),
+      dateInput("date4", "Max Look Ahead (max two days from now):", 
+                value = Sys.Date()+2),
+      # Button
+      downloadButton("downloadData", "Download"),
+      tags$img(src = "vpa.jpg",height=75,width=600),
+      tags$img(src = "SMRU.png",height=144,width=555)
       
-      sidebarPanel(
-            # Input: Choose dataset ----
-
-            selectInput("dataset", "Choose a dataset:",
-                        choices = metaDf$locs),
-            dateInput("date4", "Max Look Ahead (max two days from now):", 
-                                   value = Sys.Date()+2),
-            # Button
-            downloadButton("downloadData", "Download"),
-            tags$img(src = "vpa.jpg",height=75,width=600),
-            tags$img(src = "SMRU.png",height=144,width=555)
-            
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-
-           # Output: Tabset w/ plot, summary, and table ----
-           tabsetPanel(type = "tabs",
-                       tabPanel("Plot", 
-                                plotOutput("magPlot"), 
-                                plotOutput("dirPlot"),),
-                       tabPanel("ExportData", tableOutput("table"))
-                                
-           )
-
-        )
-    )
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      # Output: Tabset w/ plot, summary, and table ----
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", 
+                           plotOutput("magPlot"), 
+                           plotOutput("dirPlot"),),
+                  tabPanel("ExportData", tableOutput("table")))))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
   # Print the time in hh:mm:ss everytime it changes
-    output$CurrentTime<-
-      renderText(
-        paste(
+  output$CurrentTime<-
+    renderText(
+      paste(
         as.character(paste(Sys.Date(),'T', '00:00:00Z', sep='\n')),
         as.character(Sys.time())), sep='\n')
-
+  
+  
+  # Figure out whether a day needs to be subtracted
+  currentDate<-reactive({now('utc')})
+  
+  # Pull the data from the website
+  updateData<-reactive({
     
-    # Figure out whether a day needs to be subtracted
-    currentDate<-reactive({now('utc')})
     
-    # Pull the data from the website
-    updateData<-reactive({
-   
-      
-      URL =getURL(input, metaDf)
-      
-      data=fread(URL)
-      
-      #data=fread(URL())
+    URL =getURL(input, metaDf)
+    
+    data=fread(URL)
+    
+    #data=fread(URL())
     
     
     
@@ -117,81 +109,115 @@ server <- function(input, output) {
     # Calculate current magnitude and direction
     data$currentMag = sqrt(data$VelEast10^2+data$VelNorth10^2)
     
-    
     data$currentMag<-data$currentMag*mps2knots
-    
-
-    
     
     colnames(data)[1:7]<-c('time','gridY', 'gridX','VelE_mps','VelN_mps','time1','Mag_kn')
     Heading_deg<-currentDirection(data)
     data$Heading_deg<-Heading_deg
     data$Pacific =format(data$time, usetz=TRUE, tz="America/Vancouver")
     
-
+    
     return(data)})
+  
+  
+  # Create the plot of the tidal predictions
+  output$magPlot <- renderPlot({
+    
+    coeff = 90
+    data1 = data.frame(updateData())
     
     
-    # Create the plot of the tidal predictions
-    output$magPlot <- renderPlot({
-        
-        coeff = 90
-        data1 = data.frame(updateData())
+    splineData =data.frame(spline(data1$time1,data1$Mag_kn))
+    splineData$time = seq(min(data1$time1), max(data1$time1),length.out=nrow(splineData))
+    splineData$Pacific = as.POSIXct(
+      format(splineData$time, usetz=TRUE, tz="America/Vancouver"))
+    data2 = data.frame((spline(data1$time1, data1$Mag_kn)))
+    
+    
+    
+    splineData$Mag = data2$y
+    verticalLineLoc =as.Date(min(data1$time1))+days(1)
+    
+    minDate = as.POSIXct(format(min(data1$time1),
+                                usetz=TRUE, tz="America/Vancouver"))
+    
+    maxDate = as.POSIXct(format(max(data1$time1),
+                                usetz=TRUE, tz="America/Vancouver"))
+    hour(minDate)=0
+    minute(minDate)=0
+    second(minDate)=0
+    
+    hour(maxDate)=23
+    minute(maxDate)=59
+    second(maxDate)=59
+    
+    # Time breaks
+    Majorbreaks =seq(minDate, 
+                     maxDate, by = "6 hour")
+    Minorbreaks =seq(minDate, 
+                     maxDate, by = "2 hour")
+    
+    ggplot() +
+      geom_line(data= splineData, 
+                aes(y=Mag,x=Pacific), color='red') +            
+      geom_point(data=data1,
+                 aes(
+                   x=as.POSIXct(format(time1, 
+                                       usetz=TRUE, tz="America/Vancouver")),
+                   y=Mag_kn), color ='red')  +
+      theme_bw()+
+      theme( panel.grid.major.x = element_blank(),
+             panel.grid.minor.x = element_blank(),
+             axis.line = element_line(colour = "black"),
+             axis.text.x = element_text(size=14),
+             axis.text.y=element_text(size=14),
+             axis.title.y = element_text(size = 16),
+             axis.title.x= element_text(size =16)
+             )+
+    
+       ggtitle(as.character(paste(Sys.Date(),'T', '00:00:00Z', sep='')))+
+       xlab('Prediction Time (Pacific)')+
+       ylab('Current Magnitude (knots)')+
+      geom_vline(xintercept =Majorbreaks, colour="grey40", size=0.8)+
+      geom_vline(xintercept =Minorbreaks, colour="grey95", size=0.7)+
+     geom_vline(xintercept = as.POSIXct(format(
+        verticalLineLoc,usetz=TRUE, tz="America/Vancouver")), 
+        color = "blue",  lwd = 1) 
+    
 
-
-        splineData =data.frame(spline(data1$time1,
-                                      data1$Mag_kn))
-        splineData$time = seq(min(data1$time1), 
-                              max(data1$time1),
-                              length.out=nrow(splineData))
-        splineData$Pacific = as.POSIXct(
-          format(splineData$time, usetz=TRUE, tz="America/Vancouver"))
-        
-        data2 = data.frame((spline(data1$time1, 
-                                   data1$Mag_kn)))
-        splineData$Mag = data2$y
-        
-        ggplot() +
-
-          geom_line(data= splineData, 
-                    aes(y=Mag,x=Pacific), color='red') +            
-              geom_point(data=data1,
-                      aes(x=as.POSIXct(
-                        format(time1, usetz=TRUE, tz="America/Vancouver")),
-                        y=Mag_kn), color ='red') + 
- 
-            theme_bw()+
-          
-          scale_x_datetime(date_breaks = "6 hours", 
-                           date_labels = "%m/%d %H:%M",
-                           minor_breaks = '2 hours')+
-          theme(panel.grid.minor = element_line(size = 0.25),
-                panel.grid.major = element_line(size = .6,
-                                                color = 'gray80'),
-                axis.text.x = element_text(size=14),
-                axis.text.y = element_text(size=14),
-                axis.title.y = element_text(size = 16),
-                axis.title.x= element_text(size =16))+
-          ggtitle(as.character(paste(Sys.Date(),'T', '00:00:00Z', sep='')))+
-          xlab('Prediction Time (Pacific)')+
-          ylab('Current Magnitude (knots)')
-                
-        
-    })
     
-    output$dirPlot <- renderPlot({
-      data1 = data.frame(updateData())
-    
-    
-    splineData =data.frame(spline(data1$time1,
-                                  data1$Heading_deg))
-    splineData$time = seq(min(data1$time1), 
-                          max(data1$time1),
+  })
+  
+  # Create the direction predictions
+  output$dirPlot <- renderPlot({
+    data1 = data.frame(updateData())
+    verticalLineLoc =as.Date(min(data1$time1))+days(1)
+    splineData =data.frame(spline(data1$time1,data1$Heading_deg))
+    splineData$time = seq(min(data1$time1), max(data1$time1),
                           length.out=nrow(splineData))
     splineData$Pacific = as.POSIXct(
       format(splineData$time, usetz=TRUE, tz="America/Vancouver"))
     
     colnames(splineData)[2]<-'direction'
+    minDate = as.POSIXct(format(min(data1$time1),
+                                usetz=TRUE, tz="America/Vancouver"))
+    
+    maxDate = as.POSIXct(format(max(data1$time1),
+                                usetz=TRUE, tz="America/Vancouver"))
+    hour(minDate)=0
+    minute(minDate)=0
+    second(minDate)=0
+    
+    hour(maxDate)=23
+    minute(maxDate)=59
+    second(maxDate)=59
+    
+    # Time breaks
+    Majorbreaks =seq(minDate, 
+               maxDate, by = "6 hour")
+    Minorbreaks =seq(minDate, 
+                     maxDate, by = "2 hour")
+  
     
     ggplot()+
       geom_line(data= splineData, 
@@ -200,56 +226,68 @@ server <- function(input, output) {
                  aes(x=as.POSIXct(
                    format(time1, usetz=TRUE, tz="America/Vancouver")),
                    y=Heading_deg), color ='Black') + 
-      theme_bw()+
-      
-      scale_x_datetime(date_breaks = "6 hours", 
-                       date_labels = "%m/%d %H:%M",
-                       minor_breaks = '2 hours')+
-      theme(panel.grid.minor = element_line(size = 0.25),
-            panel.grid.major = element_line(size = .6,
-                                            color = 'gray80'),
+  
+      theme(axis.ticks.x=element_blank()) + #Hides ALL ticks!
+      theme(
+        axis.ticks.x=element_blank(),
+        panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black"),
             axis.text.x = element_text(size=14),
             axis.text.y = element_text(size=14),
             axis.title.y = element_text(size = 16),
             axis.title.x= element_text(size =16))+
       xlab('Prediction Time (Pacific)')+
-      ylab('Current Heading (deg)')
+      ylab('Current Heading (deg)')+
+      ylim(c(0,400))+
+      scale_y_continuous(breaks=seq(0,360,by=45), 
+                         labels = c('0-N','45-NE','90-E', 
+                                    '135-SE', '180-S', '225-SW',
+                                    '270-W', '315-NW', '360-N'))+
+      geom_hline(yintercept = seq(0,360,by=45), colour="grey90", size=0.7)+
+      geom_vline(xintercept =Majorbreaks, colour="grey40", size=0.8)+
+      geom_vline(xintercept =Minorbreaks, colour="grey95", size=0.7)+
+        geom_vline(xintercept = as.POSIXct(format(
+        verticalLineLoc,usetz=TRUE, tz="America/Vancouver")), 
+        color = "blue",  lwd = 1) 
     
+    
+  })
+  
+  # Display the data
+  # Generate an HTML table view of the data ----
+  output$table <- renderTable({
+    dataOut<- updateData()
+    
+    dataOut$Pacific = dataOut$time1
+    dataOut$Pacific =format(dataOut$Pacific, usetz=TRUE, tz="America/Vancouver")
+    dataOut$Heading_deg= as.integer(dataOut$Heading_deg)
+    dataOut<-dataOut[,c('Pacific','Mag_kn', 'Heading_deg')]
+    
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset,'_',Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
       
-    })
-    
-    # Display the data
-    # Generate an HTML table view of the data ----
-    output$table <- renderTable({
-       dataOut<- updateData()
-       
-       dataOut$Pacific = dataOut$time1
-       dataOut$Pacific =format(dataOut$Pacific, usetz=TRUE, tz="America/Vancouver")
-       
-       dataOut<-dataOut[,c('Pacific','Mag_kn', 'Heading_deg')]
-       })
-    
-    # Downloadable csv of selected dataset ----
-    output$downloadData <- downloadHandler(
-        filename = function() {
-            paste(input$dataset,'_',Sys.Date(), ".csv", sep = "")
-        },
-        content = function(file) {
-          
-          dataOut = updateData()
-          dataOut$Pacific = dataOut$time1
-          dataOut$Pacific =format(dataOut$Pacific, usetz=TRUE, 
-                                  tz="America/Vancouver")
-          
-          dataOut<-dataOut[,c('Pacific','Mag_kn', 'Heading_deg')]
-          
-          
-          dataOut<-dataOut[,c('Pacific','Mag_kn', 'Heading_deg')]
-            write.csv(dataOut, file, row.names = FALSE)
-        }
-    )
-    
-    
+      dataOut = updateData()
+      dataOut$Pacific = dataOut$time1
+      dataOut$Pacific =format(dataOut$Pacific, usetz=TRUE, 
+                              tz="America/Vancouver")
+      
+      dataOut<-dataOut[,c('Pacific','Mag_kn', 'Heading_deg')]
+      dataOut$Heading_deg= as.integer(dataOut$Heading_deg)
+      
+      dataOut<-dataOut[,c('Pacific','Mag_kn', 'Heading_deg')]
+      write.csv(dataOut, file, row.names = FALSE)
+    }
+  )
+  
+  
 }
 
 # Run the application 
